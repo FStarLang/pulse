ea5d963d238ee1d667d488bdc32d600b38bcbaf5
Juuso Haavisto <juuso@ponkila.com>
2024-11-28 11:28:12 +0000

    nix: init

---
+++ flake.nix
@@ -0,0 +1,97 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    fstar.url = "github:FStarLang/FStar/a94456863e3f971a7c63a64aca1a07d2cd9eb9a1";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, ... }@inputs:
+
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } rec {
+
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem = { pkgs, config, system, ... }: {
+
+        treefmt.config = {
+          projectRootFile = "flake.nix";
+          flakeFormatter = true;
+          flakeCheck = true;
+          programs = {
+            nixpkgs-fmt.enable = true;
+            deadnix.enable = true;
+            statix.enable = true;
+          };
+        };
+
+        devShells = {
+          default = inputs.devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                # https://devenv.sh/reference/options/
+                packages = [
+                  inputs.fstar.packages.${system}.z3
+                ] ++ (with inputs.fstar.packages.${system}.ocamlPackages; [
+                  menhir
+                  batteries
+                  menhirLib
+                  pprint
+                  ppx_deriving
+                  ppx_deriving_yojson
+                  ppxlib
+                  process
+                  sedlex
+                  stdint
+                  yojson
+                  zarith
+                ]);
+
+                env.OCAMLPATH = "${inputs.fstar.packages.${system}.fstar}/lib/ocaml/4.14.1/site-lib";
+                env.PULSE_HOME = ".";
+                enterShell = ''
+                  export PATH="${inputs.fstar.packages.${system}.fstar}/bin:$PATH"
+                '';
+
+                languages.ocaml = {
+                  enable = true;
+                  packages = inputs.fstar.packages.${system}.ocamlPackages;
+                };
+
+                scripts.pulse.exec = ''
+                  fstar.exe $1 \
+                    --include ./lib/pulse/lib/class \
+                    --include ./lib/pulse/lib/pledge \
+                    --include ./lib/pulse/lib/pulse \
+                    --include ./lib/pulse/lib/pulse/core \
+                    --include ./lib/pulse/lib/pulse/lib \
+                    --load_cmxs lib/pulse/pulse
+                '';
+              }
+            ];
+          };
+        };
+
+        packages = {
+          devenv-up = self.devShells.${system}.default.config.procfileScript;
+          devenv-test = self.devShells.${system}.default.config.test;
+
+          inherit (inputs.fstar.packages.${system}) fstar fstar-dune;
+        };
+
+      };
+    };
+}
